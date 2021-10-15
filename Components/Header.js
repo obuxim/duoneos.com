03fb21a5f6c25471a8019ce48b3192e46f459752
Zubair Hasan <z@duoneos.com>
2021-10-16 02:12:27 +0600

    Navbar Completed

---
+++ Components/Header.js
@@ -0,0 +1,42 @@
+import { React, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '@images/logo.png';
+import styled from '@styles/Header.module.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggleNavBar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <nav className='bg-duo'>
+      <div className='container flex justify-between align-center'>
+        <Link href='/'>
+          <a>
+            <Image src={Logo} width={193} height={46} alt='Duoneos' />
+          </a>
+        </Link>
+        <div className='flex align-center'>
+          <div className='d-none'>
+            <Link href='/get-quote'>
+              <a className={styled.get_quote}>get quote</a>
+            </Link>
+          </div>
+          <div className={styled.navToggle}>
+            {navbarOpen ? (
+              <FaTimes onClick={toggleNavBar} size={28} />
+            ) : (
+              <FaBars onClick={toggleNavBar} size={28} />
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ Components/Layout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
+const Layout = ({ title, description, keywords, children }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+        <meta name='author' content='Zubair Hasan' />
+        <meta copyright='Duoneos' />
+        <title>{title} | Duoneos - Next generation web development company.</title>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Oxanium:wght@400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+        <link rel='stylesheet' type='text/css' href='/css/styles.css' />
+      </Head>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
